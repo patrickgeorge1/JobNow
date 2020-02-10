a3e0b05d5876e041362ad9b65027b03690e80a6b
patrickgeorge1 <patrionpatrick@gmail.com>
2020-02-10 06:40:57 -0800

    +++

--- src/main/java/com/app/Controllers/PatrickController.java
+++ src/main/java/com/app/Controllers/PatrickController.java
@@ -1,10 +1,13 @@
 package com.app.Controllers;
 
 
+import com.app.Models.User;
+import com.app.Repository.UserRepository;
 import com.app.Services.ExempluServiciu;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,6 +18,8 @@ public class PatrickController {
 
     @Autowired
     ExempluServiciu serviciu1;
+    @Autowired
+    UserRepository userRepository;
 
 
     @RequestMapping("/")
@@ -31,5 +36,21 @@ public class PatrickController {
 
 
         return new ModelAndView("index", params);
+    }
+
+    @RequestMapping("/add")
+    public String Add() {
+        User n = new User();
+        n.setName("alin");
+        n.setEmail("sdasd@gmail.com");
+        userRepository.save(n);
+        return "Saved";
+    }
+
+    @RequestMapping(path="/all")
+    public @ResponseBody
+    Iterable<User> getAllUsers() {
+        // This returns a JSON or XML with the users
+        return userRepository.findAll();
     }
 }
--- src/main/java/com/app/JobNow/JobNowApplication.java
+++ src/main/java/com/app/JobNow/JobNowApplication.java
@@ -2,8 +2,12 @@ package com.app.JobNow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages={"com.app"})
+@EnableJpaRepositories("com.app")
+@EntityScan("com.app")
 public class JobNowApplication {
 
 	public static void main(String[] args) {

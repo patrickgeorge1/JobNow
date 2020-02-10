45b4b44e9173a18707c8fa3936df2fdade8d9414
tudor199 <tuddor144@gmail.com>
2020-02-10 21:55:46 +0200

    remove trash

--- src/main/java/com/app/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.Repository;
-
-import com.app.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
